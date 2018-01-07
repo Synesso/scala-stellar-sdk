385b8e78d11aad085a3264975ae7514f1b92ea3a
Jem Mawson <jem.mawson@gmail.com>
2018-01-07 17:33:33 +1000

    added Account

---
+++ src/test/scala/stellar/scala/sdk/AccountSpec.scala
@@ -0,0 +1,15 @@
+package stellar.scala.sdk
+
+import org.specs2.mutable.Specification
+
+class AccountSpec extends Specification with ArbitraryInput {
+
+  "an account" should {
+    "provide the successive version of itself" >> prop { account: Account =>
+      val next = account.withIncrementedSequentNumber
+      next.sequenceNumber mustEqual account.sequenceNumber + 1
+      next.keyPair mustEqual account.keyPair
+    }
+  }
+
+}
--- src/test/scala/stellar/scala/sdk/ArbitraryInput.scala
+++ src/test/scala/stellar/scala/sdk/ArbitraryInput.scala
@@ -6,7 +6,13 @@ import org.specs2.ScalaCheck
 trait ArbitraryInput extends ScalaCheck {
 
   implicit def arbKeyPair: Arbitrary[KeyPair] = Arbitrary(genKeyPair)
+  implicit def arbAccount: Arbitrary[Account] = Arbitrary(genAccount)
 
   def genKeyPair: Gen[KeyPair] = Gen.oneOf(Seq(KeyPair.random))
+
+  def genAccount: Gen[Account] = for {
+    kp <- genKeyPair
+    seq <- Gen.posNum[Long]
+  } yield Account(kp, seq)
 
 }
