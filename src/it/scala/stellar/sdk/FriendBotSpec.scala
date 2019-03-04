063d29dcd1441722a443c0b7208a9ff79203e63c
Jem Mawson <jem.mawson@gmail.com>
2019-03-04 14:50:03 +1000

    friendbot is fixed

--- src/it/scala/stellar/sdk/FriendBotSpec.scala
+++ src/it/scala/stellar/sdk/FriendBotSpec.scala
@@ -17,8 +17,8 @@ class FriendBotSpec(implicit ee: ExecutionEnv) extends Specification {
       // #friendbot_example
 
       response.map(_.isSuccess must beTrue).awaitFor(1 minute)
-    }.pendingUntilFixed("It's out of funds")
-
+    }
+    
     "be used to serialise a transaction" >> {
       val accn = KeyPair.fromPassphrase("an account")
       val sequence = 1
