22723f5eb0af5d715deee03e6529b8b7aeed5efb
Jem Mawson <jem.mawson@gmail.com>
2020-08-16 10:22:43 +1000

    Includes home domain as an explicit parameter when constructing sep10 challenge (#323)
    Co-authored-by: Jem Mawson <jmawson@squareup.com>

--- src/test/scala/stellar/sdk/auth/ChallengeSpec.scala
+++ src/test/scala/stellar/sdk/auth/ChallengeSpec.scala
@@ -20,19 +20,19 @@ class ChallengeSpec extends Specification with DomainMatchers with ArbitraryInpu
   "a generated authentication challenge" should {
     "have the source account set the the server's signing account" >> {
       val clientKey = KeyPair.random
-      val challenge = subject.challenge(clientKey.toAccountId)
+      val challenge = subject.challenge(clientKey.toAccountId, "test.com")
       challenge.transaction.source.id mustEqual serverKey.toAccountId
     }
 
     "have a sequence number of zero" >> {
       val clientKey = KeyPair.random
-      val challenge = subject.challenge(clientKey.toAccountId)
+      val challenge = subject.challenge(clientKey.toAccountId, "test.com")
       challenge.transaction.source.sequenceNumber mustEqual 0
     }
 
     "default to a timeout of the recommended 15 minutes" >> {
       val clientKey = KeyPair.random
-      val challenge = subject.challenge(clientKey.toAccountId)
+      val challenge = subject.challenge(clientKey.toAccountId, "test.com")
       challenge.transaction.timeBounds.end must beLike { when: Instant =>
         when.toEpochMilli must beCloseTo(Instant.now().plusSeconds(900).toEpochMilli, 5_000)
       }
@@ -40,7 +40,7 @@ class ChallengeSpec extends Specification with DomainMatchers with ArbitraryInpu
 
     "include the actual timeout if one is specified" >> {
       val clientKey = KeyPair.random
-      val challenge = subject.challenge(clientKey.toAccountId, timeout = 3.minutes)
+      val challenge = subject.challenge(clientKey.toAccountId, "test.com", timeout = 3.minutes)
       challenge.transaction.timeBounds.end must beLike { when: Instant =>
         when.toEpochMilli must beCloseTo(Instant.now().plusSeconds(180).toEpochMilli, 2_000)
       }
@@ -48,7 +48,7 @@ class ChallengeSpec extends Specification with DomainMatchers with ArbitraryInpu
 
     "have a single manage data operation from the challenged account" >> {
       val clientKey = KeyPair.random
-      val challenge = subject.challenge(clientKey.toAccountId)
+      val challenge = subject.challenge(clientKey.toAccountId, "test.com")
       challenge.transaction.operations.size mustEqual 1
       challenge.transaction.operations.head must beLike[Operation] { case op: WriteDataOperation =>
         op.sourceAccount must beSome(clientKey.asPublicKey)
@@ -57,45 +57,37 @@ class ChallengeSpec extends Specification with DomainMatchers with ArbitraryInpu
 
     "be signed by the provided server key" >> {
       val clientKey = KeyPair.random
-      val challenge = subject.challenge(clientKey.toAccountId)
+      val challenge = subject.challenge(clientKey.toAccountId, "test.com")
       challenge.signedTransaction.signatures.size mustEqual 1
       challenge.signedTransaction.verify(serverKey) must beTrue
     }
 
     "json serialise and deserialise" >> prop { clientKey: KeyPair =>
-      val challenge = subject.challenge(clientKey.toAccountId)
+      val challenge = subject.challenge(clientKey.toAccountId, "test.com")
       Challenge(challenge.toJson) must beEquivalentTo(challenge)
     }
   }
 
   "the operation in the generated authentication challenge" should {
-    "have the key 'auth' if none was specified" >> {
+    "have the key '<home domain> auth'" >> {
       val clientKey = KeyPair.random
-      val challenge = subject.challenge(clientKey.toAccountId)
+      val challenge = subject.challenge(clientKey.toAccountId, "test.com")
       challenge.transaction.operations.head must beLike[Operation] { case op: WriteDataOperation =>
-        op.name mustEqual "auth"
+        op.name mustEqual "test.com auth"
       }
     }
 
-    "have the given key, if one was specified" >> {
-      val clientKey = KeyPair.random
-      val challenge = subject.challenge(clientKey.toAccountId, dataKey = "Fancy auth key")
-      challenge.transaction.operations.head must beLike[Operation] { case op: WriteDataOperation =>
-        op.name mustEqual "Fancy auth key"
-      }
-    }
-
-    "disallow keys greater than 64 characters" >> {
+    "disallow home domains greater than 59 characters" >> {
       val clientKey = KeyPair.random
       subject.challenge(
         clientKey.toAccountId,
-        dataKey = "." * 65
+        homeDomain = "." * 60
       ) should throwAn[IllegalArgumentException]
     }
 
     "have a cryptographic random 48 byte value" >> {
       val clientKey = KeyPair.random
-      val challenge = subject.challenge(clientKey.toAccountId)
+      val challenge = subject.challenge(clientKey.toAccountId, "test.com")
       challenge.transaction.operations.head must beLike[Operation] { case op: WriteDataOperation =>
         op.value must haveSize(48)
       }
